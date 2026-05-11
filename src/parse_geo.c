cbd2f55520f10f3bbd5cb447c995ace604cdbbe3
Felilepe <felipesepulveda536@gmail.com>
2026-05-11 00:37:13 -0300

    fix: corrige vazamento de memória

--- src/parse_geo.c
+++ src/parse_geo.c
@@ -60,16 +60,15 @@ quadraSetBordaWidth(q, reg.sw);
 quadraSetCorBorda(q,   reg.cstrk);
 quadraSetCorPreench(q, reg.cfill);
 svgInsertQuadra(svg, q);
-    // svgMarcaCep(svg, q);quadraDestroy(q);
+quadraDestroy(q);
 }
+
 static void processar_cq(const char *linha)
 {
     char cfill[30], cstrk[30];
     double sw;
 
-    int lidos = sscanf(linha,
-                       "%*s %lf%*s %29s %29s",
-                       &sw, cfill, cstrk);
+    int lidos = sscanf(linha, "%*s %lf %29s %29s", &sw, cfill, cstrk);
 
     if (lidos != 3) {
         fprintf(stderr, "AVISO: linha 'cq' malformada: %s", linha);
